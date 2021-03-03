cb7256eb8c6a03e64d385ad92ad9eb8abe654ac9
Daniel Menelkir <menelkir@itroll.org>
2021-03-03 09:56:14 -0300

    modified:   Makefile.am 	modified:   libg15render.h

--- libg15render.h
+++ libg15render.h
@@ -13,7 +13,7 @@ extern "C"
     #endif
     
     #ifdef TTF_SUPPORT
-    #include <ft2build.h>
+    #include <freetype2/ft2build.h>
     #include FT_FREETYPE_H
     #include FT_BITMAP_H
     #endif
